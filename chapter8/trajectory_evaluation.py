981c1471fe05e25846a77a1884fec63b0f3cf3ab
Ben Auffarth <auffarth@gmail.com>
2025-03-18 14:06:39 +0000

    remove python scripts

--- chapter8/trajectory_evaluation.py
+++
@@ -1,6 +0,0 @@
-from langchain.evaluation import EvaluatorType, load_evaluator
-from langchain_openai import OpenAI
-
-eval_llm = OpenAI(temperature=0)
-# GPT 4.0 by default:
-evaluator = load_evaluator(evaluator=EvaluatorType.AGENT_TRAJECTORY, llm=eval_llm)
